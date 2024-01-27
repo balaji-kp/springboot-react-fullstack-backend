5864b1c230bbf1a26001eaf645ded014f50da3f0
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-01-27 13:40:33 +0530

    Update UserController.java

--- src/main/java/com/codewitharjun/fullstackbackend/controller/UserController.java
+++ src/main/java/com/codewitharjun/fullstackbackend/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 /* Created by Arjun Gautam */
 @RestController
-@CrossOrigin("${FRONTEND_ENDPOINT}")
+@CrossOrigin("http://${FRONTEND_ENDPOINT}")
 public class UserController {
 
     @Autowired
